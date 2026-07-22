171365d9c2a9f02a56fc961e56585fdeada35159
Yoshimasa Sagane <y3sagane@nodai.ac.jp>
2026-07-22 20:58:05 +0900

    Add distractor generation to quiz engine

--- js/quiz-engine.js
+++ js/quiz-engine.js
@@ -9,3 +9,15 @@ import { shuffle } from "./common.js";
 export function makeQueue(compounds, count = 10) {
   return shuffle(compounds).slice(0, count);
 }
+
+export function distract(all, a) {
+  const same = shuffle(
+    all.filter(c => c.id !== a.id && c.class_group === a.class_group)
+  ).slice(0, 2);
+
+  const other = shuffle(
+    all.filter(c => c.id !== a.id && !same.some(x => x.id === c.id))
+  ).slice(0, 3 - same.length);
+
+  return shuffle([a, ...same, ...other]);
+}
